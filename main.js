0ab6384e9b8e45543d675fcdc2a91f4f5cab3620
CarinaGood <carina.cgood@gmail.com>
2022-11-30 18:51:25 +0100

    Token not there draw cricle

--- main.js
+++ main.js
@@ -22,6 +22,14 @@ function listenToTokens() {
     const normalizedXPos = window.innerWidth * data.x;
     const normalizedYPos = window.innerHeight * data.y;
 
+    //console.log(data.id);
+
+    let token = document.querySelector("div.token");
+    token.style.opacity = "0";
+
+    let landcover = document.querySelector("div.links");
+    landcover.style.opacity = "1";
+
     moveLandcover(normalizedXPos, normalizedYPos);
     selectLandcover(data.rotation, normalizedXPos, normalizedYPos);
 
@@ -108,74 +116,4 @@ function selectLandcover(degrees, x, y){
 
 }
 
-function drawSVG() {
-  
-  // creating an svg within the div with id #d3
-  const svg = d3
-    .select("#d3")
-    .append("svg")
-    // setting svg specific attributes for the svg tag
-    .attr("width", window.innerWidth)
-    .attr("height", window.innerHeight);
-
-  // creating a <g> group tag
-  const indicatorGroup = svg.append("g").attr("id", "indicator");
-  const rotatingGroup = indicatorGroup.append("g").attr("id", "rotate");
-
-  const triangle = d3.symbol().type(d3.symbolTriangle).size(settings.triangleSize);
-
-  // using the same svg selection from before and adding a line
-  rotatingGroup
-    .append("circle")
-    .style("stroke", "#0022ff")
-    .style("stroke-width", settings.strokeWidth)
-    .style("fill", "transparent")
-
-    .attr("r", settings.circleSize)
-    .attr("cx", 0)
-    .attr("cy", 0);
-
-  rotatingGroup
-    .append("path")
-    .attr("d", triangle)
-    .attr("stroke", "#0022ff")
-    .attr("fill", "#0022ff")
-    .attr("transform", `translate(0, -${settings.circleSize + settings.strokeWidth})`);
-
-  indicatorGroup
-    .append("text")
-    .attr("id", "indicator-text")
-    .attr("font-size", settings.fontSize)
-    .attr("text-anchor", "middle")
-    .attr("x", 0)
-    .attr("y", 0)
-    .attr("fill", "#000")
-    .text("0°");
-}
-
-function updateText(text) {
-  const circleText = d3.select("#indicator-text").text(`${text} °`);
-}
-
-function moveSVG(x, y) {
-  const indicator = d3
-    .select("#indicator")
-    .transition()
-    .duration(500)
-    .ease(d3.easeLinear)
-    .attr("transform", () => `translate(${x}, ${y})`);
-}
-
-function rotateSVG(degrees) {
-  const rotator = d3
-    .select("#rotate")
-    .transition()
-    .duration(500)
-    .ease(d3.easeLinear)
-    .attr("transform", () => `rotate(${degrees})`);
-}
-
-
-//loadData();
-//drawSVG();
 listenToTokens();
