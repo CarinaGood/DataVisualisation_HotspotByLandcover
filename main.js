6f9b562b842c1301846ded9ec6f4d8fd2e835cca
CarinaGood <carina.cgood@gmail.com>
2022-11-30 17:50:35 +0100

    selection with rotation

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ function listenToTokens() {
     const normalizedYPos = window.innerHeight * data.y;
 
     moveLandcover(normalizedXPos, normalizedYPos);
-    selectLandcover(data.rotation);
+    selectLandcover(data.rotation, normalizedXPos, normalizedYPos);
 
   });
 }
@@ -41,7 +41,7 @@ function moveLandcover(x, y){
   )
 }
 
-function selectLandcover(degrees){
+function selectLandcover(degrees, x, y){
   let matches = document.querySelectorAll("li");
   //console.log(matches[3]);
   // to-do remove selection
@@ -51,42 +51,60 @@ function selectLandcover(degrees){
     }
   )
 
+  degrees = 36*1-1;
+
   switch(true) {
     case degrees < 36:
       matches[0].classList.add("links__link_selected");
+      matches[0].parentElement.style.transformOrigin = `${x}px ${y}px`;
+      matches[0].parentElement.style.transform = `rotate(${3*36}deg)`;
       break;
     case degrees < 36*2:
       matches[1].classList.add("links__link_selected");
+      matches[0].parentElement.style.transformOrigin = `${x}px ${y}px`;
+      matches[0].parentElement.style.transform = `rotate(${2*36}deg)`;
       break;
     case degrees < 36*3:
       matches[2].classList.add("links__link_selected");
+      matches[0].parentElement.style.transformOrigin = `${x}px ${y}px`;
+      matches[0].parentElement.style.transform = `rotate(${1*36}deg)`;
       break;
     case degrees < 36*4:
       matches[3].classList.add("links__link_selected");
+      matches[0].parentElement.style.transformOrigin = `${x}px ${y}px`;
+      matches[0].parentElement.style.transform = `rotate(${0*36}deg)`;
       break;
     case degrees < 36*5:
       matches[4].classList.add("links__link_selected");
+      matches[0].parentElement.style.transformOrigin = `${x}px ${y}px`;
+      matches[0].parentElement.style.transform = `rotate(${9*36}deg)`;
       break;
     case degrees < 36*6:
       matches[5].classList.add("links__link_selected");
+      matches[0].parentElement.style.transformOrigin = `${x}px ${y}px`;
+      matches[0].parentElement.style.transform = `rotate(${8*36}deg)`;
       break;
     case degrees < 36*7:
       matches[6].classList.add("links__link_selected");
+      matches[0].parentElement.style.transformOrigin = `${x}px ${y}px`;
+      matches[0].parentElement.style.transform = `rotate(${7*36}deg)`;
       break;
     case degrees < 36*8:
       matches[7].classList.add("links__link_selected");
+      matches[0].parentElement.style.transformOrigin = `${x}px ${y}px`;
+      matches[0].parentElement.style.transform = `rotate(${6*36}deg)`;
       break;
     case degrees < 36*9:
       matches[8].classList.add("links__link_selected");
+      matches[0].parentElement.style.transformOrigin = `${x}px ${y}px`;
+      matches[0].parentElement.style.transform = `rotate(${5*36}deg)`;
       break;
     default:
       matches[9].classList.add("links__link_selected");
+      matches[0].parentElement.style.transformOrigin = `${x}px ${y}px`;
+      matches[0].parentElement.style.transform = `rotate(${4*36}deg)`;
       break;
   }
-
-  
-
-   
 
 }
 
